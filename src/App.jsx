658c366d5b691e8945cc89151e06196b897dbd7f
Raj-Dusane <dusaneraj15@gmail.com>
2024-08-22 20:48:00 +0530

    Added count down

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+/**
+ * @author Raj Dusane <dusaneraj15@gmail.com>
+ */
+
 import React, { useEffect } from 'react';
 import leftFlower from './assets/img/pinkFlower_left.png';
 import rightFlower from './assets/img/pinkFlower_right.png';
 import midFlower from './assets/img/midFlower.png';
 import GitHub from './assets/img/github.png';
-import song from './assets/mp3/Kudmayi.mp3';
+import song from '/mp3/Kudmayi.mp3';
 import './style.css';
 
 const EngagementInvite = () => {
@@ -15,24 +19,45 @@ const EngagementInvite = () => {
     }
   };
 
-  const downloadInvitation = () => {
-    const link = document.createElement('a');
-    link.href = 'https://github.com/Raj-Dusane/Invitation/raw/main/src/assets/Invitation_Card/Invitation_Card.png';
-    link.download = 'Invitation_Card.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   useEffect(() => {
-    playAudio();
+    setTimeout(() => {
+      playAudio();
+    }, 1000);
+
     if (window.$) {
       $('.sakura-falling').sakura();
     }
-  });
+
+    const countDownDate = new Date("Aug 27, 2024 10:30:00").getTime();
+
+    const x = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance/ (1000 * 60 * 60 *24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      document.getElementById("time").innerHTML = `
+        <div class='container'>
+          <div class='days block'>${days}<br> <span class='size'> Days </span></div>
+          <div class='hours block'>${hours}<br> <span class='size'> Hours </span></div>
+          <div class='minutes block'>${minutes}<br> <span class='size'> Minutes </span></div>
+          <div class='seconds block'>${seconds}<br><span class='size'> Seconds </span></div>
+        </div>`;
+      
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Congra!";
+      }
+    }, 1000);
+
+    return () => clearInterval(x); // Clean up the interval on component unmount
+  }, []);
 
   return (
-    <div className="sakura-falling">
+    <div className="sakura-falling" onClick={playAudio}>
       <img src={rightFlower} alt="image-top-right" className="top-right-decoration" />
       <img src={leftFlower} alt="image-top-left" className="top-left-decoration" />
       
@@ -57,13 +82,9 @@ const EngagementInvite = () => {
           <a href="https://www.google.com/maps/place/Ekadashi+Pure+Veg+%26+Banquet+Hall/@18.5790497,73.8194351,1061m/data=!3m2!1e3!4b1!4m6!3m5!1s0x3bc2b9fa88974eed:0x20c3798ed8892c3e!8m2!3d18.5790497!4d73.8194351!16s%2Fg%2F11y5jydjmj?entry=ttu" target="_blank" rel="noopener noreferrer">
             <div className="venue">LOCATION</div>
           </a>
-
-          {/* href="https://drive.google.com/file/d/1QRiPq9grvYvG2aQGUR3xvrtBSvF2RH4F/view?usp=drive_link" */}
-          {/* <button onClick={downloadInvitation} className="venue"> */}
           <a href='https://github.com/Raj-Dusane/Invitation/raw/main/src/assets/Invitation_Card/Invitation_Card.png' target="_blank" type='text/plain' download="Invitation_Card.png">
             <div className="venue">DOWNLOAD INVITATION CARD</div>
           </a>
-          {/* </button> */}
         </div>
 
         <p className="footer">
@@ -76,8 +97,10 @@ const EngagementInvite = () => {
         </div>
 
         <p className="happiness"> Your presence is requested! <br />
-          <a href="https://github.com/raj-dusane" target="_blank" rel="noopener noreferrer" className="GitHub">
-          <a href="https://www.linkedin.com/in/rajdusane/" target="_blank"> &#169; Raj Dusane</a> <img src={GitHub} alt="" /> </a>
+          <div className="links">
+            <a href="https://github.com/raj-dusane" target="_blank" rel="noopener noreferrer" className="GitHub"> <img src={GitHub} alt="" /> </a>
+            <a href="https://www.linkedin.com/in/rajdusane/" target="_blank" className="linkedIn" style={{textDecoration:"None"}}> &#169; Raj Dusane</a> 
+          </div>
         </p>
       </div>
     </div>
